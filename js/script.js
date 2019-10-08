ff074b22a151e8f87692d3cde555ec2568235b5e
unknown <phuocduc2012@gmail.com>
2019-10-09 00:15:32 +0700

    maybe final

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ function getOldSession(){
         masterTodoList = JSON.parse(localStorage.getItem('todolist'))
         let transferDataFromOldStorageToHtml = masterTodoList.map(items=>items.text).join(" ")
         document.getElementById('show').innerHTML = transferDataFromOldStorageToHtml
-     
+        
         
     }
 }
@@ -21,9 +21,10 @@ function addTodo()
 {
     const item = document.getElementById('thingsToDo').value;
     const time = document.getElementById('datetime').value;
-    masterTodoList.push({
+        masterTodoList.push({
         text: `${item}`,
         isDone:false,
+        isPending: false,
         date: `${time}`
     })
     renderTodos(masterTodoList)
@@ -40,19 +41,50 @@ function renderTodos(arr)
     let html = ""
     for(let i = 0;i< arr.length;i++)
     {
-        html += `<li onclick="toggleDone(${i})" style="list-style-type: none;font-size:20pt" class="${arr[i].isDone ? 'Done': 'unDone'}">${arr[i].date}${arr[i].text}<a href="#" onclick="remove(${i})">${arr[i].isDone ? '  MarkUndone <a style="cursor:pointer">X</a>' : '   MarkDone <a style="cursor:pointer">X</a>'}</a> </li>\n`;
+        // html += `<li onclick="toggleDone(${i})" 
+        // style="list-style-type: none;font-size:20pt" 
+        // class="${arr[i].isDone ? 'Done': 'unDone'}">${arr[i].date}${arr[i].text}<a href="#" class="delete-mark"
+        // onclick="remove(${i})">delete</li>\n`;
+
+        //other ways
+
+        html += `<li onclick="toggleDone(${i})"
+        style="list-style-type: none;font-size:20pt;display:flex;justify-content:space-between" 
+        class="${arr[i].isDone ? 'Done': 'unDone'}" id="li-pending">${arr[i].date}  ${arr[i].text}
+        </li><span onclick="Opening(${i})"><a href=#">Opening<a></span>
+        <span onclick="remove(${i})" style="margin-right:40px"><a href="#">Delete</a></span> 
+        `
     }
-    console.log("test html",html)
-    document.getElementById('show').innerHTML = `<ul>${html}</ul>`
+    document.getElementById('show').innerHTML = `${html}`
     save()
 }
 
 
+function Opening(idx)
+{
+    if(masterTodoList[idx].pending == true)
+    {
+        masterTodoList[idx].pending = false
+        renderTodos(masterTodoList)
+    }
+    else{
+        masterTodoList[idx].pending = true
+        renderTodos(masterTodoList)
+        document.getElementById('li-pending').style.backgroundColor = "yellow"
+        
+    }
+}
 
 
+function openJob()
+{
+    let pending = masterTodoList.filter(items=>items.pending === true)
+    renderTodos(pending)
+}
 
 function remove(idx)
 {
+
     masterTodoList.splice(idx,1)
     renderTodos(masterTodoList)
 }
@@ -113,7 +145,7 @@ function checkBoxFilter()
 }
 
 
-var input = document.getElementById("thingsToDo");
+let input = document.getElementById("thingsToDo");
 input.addEventListener("keyup", function(event) {
   if (event.keyCode === 13) {
    event.preventDefault();
